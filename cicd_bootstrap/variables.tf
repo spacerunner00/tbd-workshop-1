11e8612d7ffc6149adb15801b05447949e06e37d
spacerunner00 <aleksmoonrunner.00@gmail.com>
2025-01-28 18:33:39 +0000

    cicd files update

--- cicd_bootstrap/variables.tf
+++ cicd_bootstrap/variables.tf
@@ -11,15 +11,18 @@ variable "region" {
 
 variable "iac_service_account" {
   type        = string
+  default     = "tbd-2024zz-305978-lab@tbd-2024zz-305978.iam.gserviceaccount.com"
   description = "Service account to be used with CI/CD workload identity"
 }
 
 variable "github_repo" {
   type        = string
+  default     = "tbd-workshop-1"
   description = "Github repository"
 }
 
 variable "github_org" {
   type        = string
+  default     = "spacerunner00"
   description = "Github organisation"
 }
